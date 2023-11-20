4e096a4d3bd56f1da2b110ab4f780c3297c1b745
starishsky <117216668+starishsky@users.noreply.github.com>
2023-11-20 11:16:54 -0500

    sigh

--- Yanr/Assets/scripts/playerController.cs
+++ Yanr/Assets/scripts/playerController.cs
@@ -9,15 +9,22 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
 
     private CharacterController characterController;
+    public Animator animator;
 
     float inputHorizontal;
     float inputVertical;
 
-    public float Speed = 5f;
+    bool facingRight;
+    bool facingLeft;
+    bool isWalking;
+
+    public float Speed = 3f;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        
         
     }
 
@@ -26,7 +33,6 @@ public class playerController : MonoBehaviour
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-
         characterController.Move(move * Time.deltaTime * Speed);
 
         inputHorizontal = Input.GetAxisRaw("Horizontal");
@@ -34,12 +40,28 @@ public class playerController : MonoBehaviour
 
         if (inputHorizontal > 0)
         {
-            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            // gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("facingRight", true);
+            animator.SetBool("facingLeft", false);
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("inputHorizontal", inputHorizontal);
+            
         }
 
-        if (inputHorizontal < 0)
+
+        else if (inputHorizontal < 0)
         {
-            gameObject.transform.localScale = new Vector3(-1f, 1f, 1f);
+            // gameObject.transform.localScale = new Vector3(-1f, 1f, 1f);
+            animator.SetBool("facingLeft", true);
+            animator.SetBool("facingRight", false);
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("inputHorizontal", inputHorizontal);
+        
+        }
+
+        else
+        {
+            animator.SetBool("isWalking", false);
         }
 
         
