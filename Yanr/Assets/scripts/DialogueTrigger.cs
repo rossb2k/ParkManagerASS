63bf12149438f42f0a6d075d8d1a8221eadbcfc2
starishsky <117216668+starishsky@users.noreply.github.com>
2023-12-09 15:22:41 -0500

    aaaa

---
+++ Yanr/Assets/scripts/DialogueTrigger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public DialogueRunner dialogueRunner;
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "visitor center")
+        {
+            Debug.Log("collision");
+            dialogueRunner.StartDialogue("Start");
+        }
+
+        if (col.gameObject.name == "wildlife center")
+        {
+            Debug.Log("collision");
+            dialogueRunner.StartDialogue("WeekTwoBigDecisions");
+            
+        }
+
+        if (col.gameObject.name == "tourism center")
+        {
+            Debug.Log("collision");
+            
+        }
+    }
+}
